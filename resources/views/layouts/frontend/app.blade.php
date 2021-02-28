90d65e7d7fdd693f132f79859c3a5791295fdb81
krishna mishra <er.krishna.mishra@gmail.com>
2021-03-01 00:02:46 +0530

    design added

--- resources/views/layouts/frontend/app.blade.php
+++ resources/views/layouts/frontend/app.blade.php
@@ -1,33 +1,31 @@
 <!doctype html>
 <html lang="en">
-<head>
-	@include('layouts.frontend.head')
-</head>
-   <body>
-      <div class="loader">
-         <div class="loader-inner">
-            <div class="bounce">
-  <div class="bounce-one"></div>
-  <div class="bounce-two"></div>
-</div>
-         </div>
-      </div>
-      <div id="wrapper" class="wrapper">
-	@include('layouts.frontend.includes.header')
-	
-	@yield('content')
+@include('layouts.frontend.head')
+<body>
+	<div class="loader">
+		<div class="loader-inner">
+			<div class="bounce">
+				<div class="bounce-one"></div>
+				<div class="bounce-two"></div>
+			</div>
+		</div>
+	</div>
+	<div id="wrapper" class="wrapper">
+		@include('layouts.frontend.includes.header')
 
-	@include('layouts.frontend.includes.footer')
- </div>
-      <script src="{{asset('frontend/js/jquery-1.12.4.min.js')}}"></script>
-      <script src="{{asset('frontend/js/bootstrap.min.js')}}"></script>
-      <script src="{{asset('frontend/js/smooth-scroll.js')}}"></script>
-      <script src="{{asset('frontend/js/venobox.js')}}"></script>
-      <script src="{{asset('frontend/js/owl.carousel.js')}}"></script>
-      <script src="{{asset('frontend/js/jarallax.min.js')}}"></script>
-      <script src="{{asset('frontend/js/jarallax-element.min.js')}}"></script>
-      <script src="{{asset('frontend/js/placeholders.min.js')}}"></script>
-      <script src="{{asset('frontend/js/instafeed.min.js')}}"></script>
-      <script src="{{asset('frontend/js/script.js')}}"></script>
-   </body>
+		@yield('content')
+
+		@include('layouts.frontend.includes.footer')
+	</div>
+	<script src="{{asset('frontend/js/jquery-1.12.4.min.js')}}"></script>
+	<script src="{{asset('frontend/js/bootstrap.min.js')}}"></script>
+	<script src="{{asset('frontend/js/smooth-scroll.js')}}"></script>
+	<script src="{{asset('frontend/js/venobox.js')}}"></script>
+	<script src="{{asset('frontend/js/owl.carousel.js')}}"></script>
+	<script src="{{asset('frontend/js/jarallax.min.js')}}"></script>
+	<script src="{{asset('frontend/js/jarallax-element.min.js')}}"></script>
+	<script src="{{asset('frontend/js/placeholders.min.js')}}"></script>
+	<script src="{{asset('frontend/js/instafeed.min.js')}}"></script>
+	<script src="{{asset('frontend/js/script.js')}}"></script>
+</body>
 </html>
--- resources/views/layouts/frontend/head.blade.php
+++ resources/views/layouts/frontend/head.blade.php
@@ -1,17 +1,15 @@
 @yield('head')
 <head>
-      <!-- Metas -->
-      <meta charset="utf-8">
-      <title>Foxeyoga - Premium Yoga and Wellness Html Template</title>
-      <meta name="description" content="">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <!-- Css -->
-      <link href="{{asset('frontend/css/bootstrap.css')}}" rel="stylesheet" type="text/css" media="all" />
-      <link href="{{asset('frontend/css/main.css')}}" rel="stylesheet" type="text/css" media="all" />
-      <link href="{{asset('frontend/css/venobox.css')}}" rel="stylesheet" type="text/css" media="all" />
-      <link href="{{asset('frontend/css/fonts.css')}}" rel="stylesheet" type="text/css" media="all" />
-      <link href="{{asset('frontend/css/owl-carousel/owl.carousel.css')}}" rel="stylesheet"  media="all"/>
-      <link href="{{asset('frontend/css/owl-carousel/owl.theme.css')}}" rel="stylesheet"  media="all" />
-      <link href="https://fonts.googleapis.com/css?family=PT+Serif:400,700" rel="stylesheet">
-      <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700" rel="stylesheet">
-   </head>
+  <meta charset="utf-8">
+  <title>Foxeyoga - Premium Yoga and Wellness Html Template</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="{{asset('frontend/css/bootstrap.css')}}" rel="stylesheet" type="text/css" media="all" />
+  <link href="{{asset('frontend/css/main.css')}}" rel="stylesheet" type="text/css" media="all" />
+  <link href="{{asset('frontend/css/venobox.css')}}" rel="stylesheet" type="text/css" media="all" />
+  <link href="{{asset('frontend/css/fonts.css')}}" rel="stylesheet" type="text/css" media="all" />
+  <link href="{{asset('frontend/css/owl-carousel/owl.carousel.css')}}" rel="stylesheet"  media="all"/>
+  <link href="{{asset('frontend/css/owl-carousel/owl.theme.css')}}" rel="stylesheet"  media="all" />
+  <link href="https://fonts.googleapis.com/css?family=PT+Serif:400,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700" rel="stylesheet">
+</head>
