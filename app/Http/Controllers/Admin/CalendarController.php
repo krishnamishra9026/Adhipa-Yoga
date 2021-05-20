bef0aecf683355e7645950e6f29ea7f775cb3d54
Laxman Mishra <laxmanmishrabbk@gmail.com>
2021-05-20 22:44:14 +0530

    update code

--- app/Http/Controllers/Admin/CalendarController.php
+++ app/Http/Controllers/Admin/CalendarController.php
@@ -13,8 +13,8 @@ class CalendarController extends Controller
         $weekDays     = Lesson::WEEK_DAYS;
         $calendarData = $calendarService->generateCalendarData($weekDays);
 
-        echo '<pre>'; print_r($weekDays); echo '</pre>'; 
-        echo '<pre>'; print_r($calendarData); echo '</pre>'; die();
+        // echo '<pre>'; print_r($weekDays); echo '</pre>'; 
+        // echo '<pre>'; print_r($calendarData); echo '</pre>'; die();
 
         return view('admin.calendar', compact('weekDays', 'calendarData'));
     }
